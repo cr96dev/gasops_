5918e4f83e06a1e75bb8ad5b4cdb5b73367fe085
Carlos Roldan <cjrh@Carloss-MacBook-Air.local>
2026-05-15 17:01:59 -0600

    feat: UI clasificar compras FEL sin estacion + link en sidebar

--- components/Layout.js
+++ components/Layout.js
@@ -30,6 +30,7 @@ const adminItems = [
 const contabilidadItems = [
   { href: '/contabilidad',        label: 'Centro contable', icon: 'M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z' },
   { href: '/contabilidad/cuentas', label: 'Catálogo cuentas', icon: 'M4 6h16M4 10h16M4 14h16M4 18h16' },
+  { href: '/contabilidad/compras-pendientes', label: 'Compras pendientes', icon: 'M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-6 9l2 2 4-4' },
 ]
 
 const OAKLAND_ID = '85da69a8-1e81-48a7-8b0d-82df9eeec15e'
